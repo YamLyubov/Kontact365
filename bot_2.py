11973c77e1b0f7a6631ef801a85dc755eb646794
Lyuba Yamshikova <YamLyubov@gmail.com>
2018-12-01 20:37:17 +0300

    теперь есть клава

--- bot_2.py
+++ bot_2.py
@@ -1,7 +1,7 @@
 import vk
 from requests import *
-from const import vk_keybord
-from logic import get_date, save_matching
+from logic import get_date, save_matching, get_button, vk_keyboard
+import json
 
 session = vk.Session(access_token = '705848ce54dd5994b7a60f04badbd790bf865ae217b1960acee583641c07faeff74da562a1775f610f4df')
 api = vk.API(session, v='5.85')
@@ -18,11 +18,25 @@ while True:
             if update['type'] == 'message_new':
                 api.messages.markAsRead(peer_id=update['object']['user_id'])
                 text = update['object']['body'].lower()
-                if 'привет' in text:
-                    name = api.users.get(user_ids=update['object']['user_id'])[0]['first_name']
-                    api.messages.send(peer_id=update['object']['user_id'],
-                                      message='Привет, %s ' % name)
-                if 'ip' in text:
+                if text == 'узнать дату':
+                    id = update['object']['user_id']
+                    with open('IP.json', 'r+', encoding='utf-8') as IP:
+                        data = json.load(IP)
+                        print(data)
+                        if id in data:
+                            date = get_date(data[id])
+                            api.messages.send(
+                                peer_id=update['object']['user_id'],
+                                message='Доступ к интернету будет'
+                                        ' прекращен в полночь %s ' % date)
+                        else:
+                            api.messages.send(
+                                peer_id=update['object']['user_id'],
+                                message='Пожалуйста введите IP адресс в формате'
+                                        ':\n ip:10.10.0.00\nСвой IP можно узнать'
+                                        ' по телефону 8512485.')
+
+                elif 'ip' in text:
                     ip = text[3:]
                     date = get_date(ip)
                     if date != '':
@@ -31,6 +45,7 @@ while True:
                         api.messages.send(peer_id=update['object']['user_id'],
                                         message='Доступ к интернету будет'
                                                 ' прекращен в полночь %s ' % date)
+                        ip
                     else:
                         api.messages.send(peer_id=update['object']['user_id'],
                                           message='К сожалению, информации про'
--- logic.py
+++ logic.py
@@ -24,3 +24,26 @@ def save_matching(id, ip):
         IP.seek(0)
         json.dump(data, IP)
         IP.truncate()
+
+
+def get_button(label, color, payload=""):
+    return {
+        "action": {
+            "type": "text",
+            "payload": json.dumps(payload),
+            "label": label
+        },
+        "color": color
+    }
+
+
+vk_keyboard = {
+    "one_time": False,
+    "buttons": [
+    [
+    get_button(label="Узнать дату", color='positive')
+    ]
+    ]
+}
+vk_keyboard = json.dumps(vk_keyboard, ensure_ascii=False).encode('utf-8')
+vk_keyboard = str(vk_keyboard.decode('utf-8'))
