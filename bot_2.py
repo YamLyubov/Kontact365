e0d66ec2dcefc96d7be5e693289293dc6f54c735
Lyuba Yamshikova <YamLyubov@gmail.com>
2018-12-03 23:41:33 +0300

    добавила еще кнопок, tg так и не работает((

--- bot_2.py
+++ bot_2.py
@@ -1,23 +1,37 @@
 import vk
-from requests import *
-from logic import get_date, save_matching, get_button, vk_keyboard
+import requests
+from logic import get_date, save_matching
+from logic import vk_keyboard, tg_token, vk_token
 import json
+import telebot
+from requests.auth import HTTPProxyAuth
 
-session = vk.Session(access_token = '705848ce54dd5994b7a60f04badbd790bf865ae217b1960acee583641c07faeff74da562a1775f610f4df')
+'''
+auth = HTTPProxyAuth("vkbot", "vkbot13042000")
+proxy = {"http": "socks5://80.211.242.87:3180"}
+'''
+bot = telebot.TeleBot(tg_token)
+session = vk.Session(access_token=vk_token)
 api = vk.API(session, v='5.85')
-longPoll = api.groups.getLongPollServer(group_id = 129298566)
+longPoll = api.groups.getLongPollServer(group_id=129298566)
 server, key, ts = longPoll['server'], longPoll['key'], longPoll['ts']
+url = "https://api.telegram.org/bot{}/".format(tg_token)
 while True:
-    longPoll = post('%s'%server, data={'act': 'a_check',
-                                         'key': key,
-                                         'ts': ts,
-                                         'wait': 25}).json()
+    longPoll = requests.post('%s'%server, data={'act': 'a_check',
+                                       'key': key,
+                                       'ts': ts,
+                                       'wait': 25}).json()
     print(longPoll)
     if len(longPoll['updates']) != 0:
         for update in longPoll['updates']:
             if update['type'] == 'message_new':
                 api.messages.markAsRead(peer_id=update['object']['user_id'])
                 text = update['object']['body'].lower()
+                '''
+                response = requests.post('%s' %url + 'sendMessage', proxies=dict(http='socks5://vkbot:vkbot13042000@80.211.242.87:3180',
+                                 https='socks5://vkbot:vkbot13042000@80.211.242.87:3180'),
+                                data={'chat_id': 255631997, 'text': text}, verify=False)
+                '''
                 if text == 'начать':
                     name = api.users.get(user_ids=update['object']['user_id'])[0]['first_name']
                     api.messages.send(peer_id=update['object']['user_id'],
@@ -37,17 +51,28 @@ while True:
                             api.messages.send(
                                 peer_id=update['object']['user_id'],
                                 message='Пожалуйста введите IP адресс в формате'
-                                        ':\n ip:10.10.0.00\nСвой IP можно узнать'
+                                        ':\n 10.10.0.00\nСвой IP можно узнать'
                                         ' по телефону 8512485.')
-
-                elif 'ip' in text:
-                    ip = text[3:]
+                elif text == 'оплата через сбербанк онлайн':
+                    api.messages.send(
+                        peer_id=update['object']['user_id'],
+                        message='Если будете платить через приложение в телефоне\n'
+                                'Следуйте пунктам:\n1. Платежи/Переводы\n'
+                                '2. Остальные услуги\n3. Поиск: ПК ОИС\n'
+                                '[Получатель: ПК ОИС-Ивангород\n'
+                                'ИНН/СЧЁТ: 4707024525 / 40703810855300000095]\n'
+                                '*И заполните:\nФИО: Фамилия Имя Отчество\n'
+                                'Адрес: Ивангород\nУлица Дом-Квартира\n'
+                                'Назначение:\nЧленские взносы 200\n'
+                                '(вместо 200 указать своё 400 или 500)')
+                elif '10.10.' in text:
+                    ip = text
                     date = get_date(ip)
                     if date != '':
                         id = update['object']['user_id']
                         save_matching(id, ip)
                         api.messages.send(peer_id=update['object']['user_id'],
-                                        message='Доступ к интернету будет'
+                                          message='Доступ к интернету будет'
                                                 ' прекращен в полночь %s ' % date)
                     else:
                         api.messages.send(peer_id=update['object']['user_id'],
--- logic.py
+++ logic.py
@@ -2,18 +2,22 @@ import xml.etree.ElementTree as etr
 import json
 
 
+vk_token = '705848ce54dd5994b7a60f04badbd790bf865ae217b1960acee583641c07faeff74da562a1775f610f4df'
+tg_token = '750626699:AAELeCDkbY3exPYhKk7nhw2aSXUkna2S-dA'
 def get_date(ip):
     tree = etr.parse('UsersTest.xml')
     root = tree.getroot()
     for elem in root:
         current_ip = elem.attrib['UserIPAddr']
-        print(current_ip)
         if current_ip == ip:
-            preresult = elem.attrib['Date2']
-            result = preresult[8:10] + '.' \
-                     + preresult[5:7] + '.' \
-                     + preresult[:4]
-            return result
+            try:
+                preresult = elem.attrib['Date2']
+                result = preresult[8:10] + '.' \
+                        + preresult[5:7] + '.' \
+                        + preresult[:4]
+                return result
+            except KeyError:
+                return ''
     return ''
 
 
@@ -40,9 +44,9 @@ def get_button(label, color, payload=""):
 vk_keyboard = {
     "one_time": False,
     "buttons": [
-    [
-    get_button(label="Узнать дату", color='positive')
-    ]
+     [get_button(label="Узнать дату", color='positive')],
+     [get_button(label="Оплата через сбербанк онлайн", color='primary')],
+     [get_button(label="Есть ли проблемы со связью", color='primary')]]
     ]
 }
 vk_keyboard = json.dumps(vk_keyboard, ensure_ascii=False).encode('utf-8')
